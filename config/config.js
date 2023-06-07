768e461d92deaed275b9f1743ae886c877cb1014
Dionisius Putra Widianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-07 20:46:04 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// path: .config/admin.js
+
+module.export = {{ env }} => {{
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '7lsPey/3cVLRdZ15bnHVKg=='),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'LF2URdrZTQzhc3eCYOOATg=='),
+  },
+});
