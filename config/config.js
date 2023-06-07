d180901c6200362b0b3bcf1bdc34a1d07646b9af
Dionisius Putra Widianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-07 21:01:40 +0700

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-// path: .config/admin.js
-module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env('API_TOKEN_SALT', '7lsPey/3cVLRdZ15bnHVKg=='),
-  },
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'LF2URdrZTQzhc3eCYOOATg=='),
-  },
-});
